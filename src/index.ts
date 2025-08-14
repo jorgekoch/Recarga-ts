b0a73bf3c26a69886359acda390223a0ef4191db
Jorge Koch <jorgeluizkoch@gmail.com>
2025-08-13 22:48:44 -0300

    health fixed

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import summaryRouter from './routes/summary-router';
 const app = express();
 app.use(json());
 
-app.get("health", (req: Request, res: Response) => {
+app.get("/health", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
