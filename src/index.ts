747e2fafb08bc3d32ceb671f63fc3a01ebdeeef1
Jorge Koch <jorgeluizkoch@gmail.com>
2025-08-14 11:11:28 -0300

    port updated

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ app.use(rechargeRouter);
 app.use(summaryRouter)
 app.use(errorHandler);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`);
 });
